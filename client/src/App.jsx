48e6da99f8ee1f7840a8b8c08daa81ad59fcb40b
ankita chowdhury <ankitachowdhury145@gmail.com>
2025-12-17 22:29:12 +0530

    feat: Add new components and assets for AI tools platform
    - Implemented AiTools component to showcase AI tools with navigation.
    - Created Footer, Hero, Navbar, Plan, Sidebar, and Testimonial components for the main layout.
    - Added pages for BlogTitles, Community, Dashboard, GenerateImages, RemoveBackground, RemoveObject, ReviewResume, and WriteArticle.
    - Introduced SVG icons for star ratings and React logo.
    - Added user group and profile images.
    - Configured Vite for React and Tailwind CSS.
    - Updated index.css for global styles and font imports.

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import  Home  from './pages/Home'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/ai' element={<Layout/>}>
+          <Route index element={<Dashboard/>} />
+          <Route path='write-article' element={<WriteArticle/>}></Route>
+          <Route path='blog-titles' element={<BlogTitles/>}></Route>
+          <Route path='generate-images' element={<GenerateImages/>}></Route>
+          <Route path='remove-background' element={<RemoveBackground/>}></Route>
+          <Route path='remove-object' element={<RemoveObject/>}></Route>
+          <Route path='review-resume' element={<ReviewResume/>}></Route>
+          <Route path='community' element={<Community/>}></Route>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
